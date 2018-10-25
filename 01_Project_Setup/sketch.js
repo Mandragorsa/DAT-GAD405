e980b0d6e96e2b577f0233524d8e6699de8db024
(s) Gabriela Pietraszek <gpietraszek@pm7-0079.uopnet.plymouth.ac.uk>
2018-10-25 12:35:28 +0100

    update

--- 01_Project_Setup/sketch.js
+++ 01_Project_Setup/sketch.js
@@ -11,7 +11,7 @@ function setup() {
 
   //Set up the frame rate (frames per second)
   //Default is 60 fps
-  frameRate(20);
+  frameRate(30);
 
   //Preferences for the text (size, and alignment)
   textSize(25);
@@ -26,8 +26,6 @@ function draw() {
   //Erase all canvas. Set the color to dark grey
   background(227, 235, 247);
   //Set the color of the text
-  fill(190, 100, 209);
-
   stroke(209, 50, 209, 40)
   strokeWeight(5);
   line(50, 50, 550, 350);
@@ -36,9 +34,22 @@ function draw() {
   line(550, 350, 550, 50);
   line(550, 350, 50, 350);
   line(550, 50, 50, 350);
+
+  fill(194, 150, 209, 60);
+  stroke(10, 170, 255, 40);
+  strokeWeight(10)
+  ellipse(300, 200, 160, 160);
+  ellipse(300, 200, 140, 140);
+  ellipse(300, 200, 120, 120);
+  ellipse(300, 200, 100, 100);
+  ellipse(300, 200, 80, 80);
+  ellipse(300, 200, 60, 60);
+  ellipse(300, 200, 40, 40);
   //Set the text message as the frame count
   //Position text in the middle of the screen
   stroke(10, 170, 255, 40);
+  strokeWeight(5);
+    fill(190, 100, 209);
  text("Frame Counter:  " + frameCount, width/2, height/2-20);
   //Set the text message as the frame rate (round it to an integer)
   //Position text in the middle of the screen
@@ -57,6 +68,10 @@ function draw() {
 
   fill(194, 150, 209);
   ellipse(550, 50, 60, 60);
+
+
+
+
 
 
 
